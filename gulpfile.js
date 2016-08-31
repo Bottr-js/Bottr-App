040e59daa247ae3a7cb94dd8f601e62cb8b9a1fc
James Campbell <james@supmenow.com>
2016-08-31 15:30:22 +0100

    React binded.

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var webpack = require('webpack-stream');
+
+gulp.task('default', function() {
+  return gulp.src('web/index.js')
+  .pipe(webpack({
+      output: {
+        filename: '[name].js',
+      },
+      module: {
+        loaders: [{
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          loader: 'babel'
+        }]
+      }
+  }))
+  .pipe(gulp.dest('web/dist/'))
+});
