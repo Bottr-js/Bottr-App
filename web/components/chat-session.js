ed762bd162a6e280a0f1b1744362f87fb9c95d11
James Campbell <james@supmenow.com>
2016-09-15 17:57:45 +0100

    Watch.

--- web/components/chat-session.js
+++ web/components/chat-session.js
@@ -8,7 +8,7 @@ class ChatSession extends EventEmitter {
 
     super()
 
-    this.socket = io(window.location.href, { origins: '*:*'})
+    this.socket = io(window.location.href)
 
     this.socket.on('message', function(msg) {
       this.emit('message', {
