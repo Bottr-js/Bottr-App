a49bfef2998f0ff6a712ca65c37ff67e6c212dcc
James Campbell <james@supmenow.com>
2016-09-15 19:17:08 +0100

    Bump.

--- web/components/typing-indicator.js
+++ web/components/typing-indicator.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 class TypingIndicator extends React.Component {
   render() {
-    return <div className="message typing"><img src="images/typing.gif"/></div>
+    return <div className="message typing"><img src="res/typing.gif"/></div>
   }
 }
 
